bd5d842bef1920c5d4765b0a994f7264d8c0c702
elsayedelhoseny <sayedelhoseny09@gmail.com>
2024-08-27 10:07:56 +0300

    Finish checkout payment ui

--- lib/Features/checkout/presentation/views/widgets/thank_you_view_body.dart
+++ lib/Features/checkout/presentation/views/widgets/thank_you_view_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:payment/Features/checkout/presentation/views/widgets/custom_check_icon.dart';
+import 'package:payment/Features/checkout/presentation/views/widgets/custom_dashed_line.dart';
+import 'package:payment/Features/checkout/presentation/views/widgets/thank_you_card.dart';
 
 class ThankYouViewBody extends StatelessWidget {
   const ThankYouViewBody({super.key});
@@ -10,31 +13,31 @@ class ThankYouViewBody extends StatelessWidget {
       child: Stack(
         clipBehavior: Clip.none,
         children: [
-          // const ThankYouCard(),
-          // Positioned(
-          //   bottom: MediaQuery.sizeOf(context).height * .2 + 20,
-          //   left: 20 + 8,
-          //   right: 20 + 8,
-          //   child: const CustomDashedLine(),
-          // ),
-          // Positioned(
-          //     left: -20,
-          //     bottom: MediaQuery.sizeOf(context).height * .2,
-          //     child: const CircleAvatar(
-          //       backgroundColor: Colors.white,
-          //     )),
-          // Positioned(
-          //     right: -20,
-          //     bottom: MediaQuery.sizeOf(context).height * .2,
-          //     child: const CircleAvatar(
-          //       backgroundColor: Colors.white,
-          //     )),
-          // const Positioned(
-          //   top: -50,
-          //   left: 0,
-          //   right: 0,
-          //   child: CustomCheckIcon(),
-          // ),
+          const ThankYouCard(),
+          Positioned(
+            bottom: MediaQuery.sizeOf(context).height * .2 + 20,
+            left: 20 + 8,
+            right: 20 + 8,
+            child: const CustomDashedLine(),
+          ),
+          Positioned(
+              left: -20,
+              bottom: MediaQuery.sizeOf(context).height * .2,
+              child: const CircleAvatar(
+                backgroundColor: Colors.white,
+              )),
+          Positioned(
+              right: -20,
+              bottom: MediaQuery.sizeOf(context).height * .2,
+              child: const CircleAvatar(
+                backgroundColor: Colors.white,
+              )),
+          const Positioned(
+            top: -50,
+            left: 0,
+            right: 0,
+            child: CustomCheckIcon(),
+          ),
         ],
       ),
     );
